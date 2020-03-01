453539aedd8ac30dbb5cb80b19a5e780cbbd6e91
Martin Caslavsky <martin.caslavsky@wandera.com>
2020-03-01 15:14:40 +0100

    initial

---
+++ src/test/java/cz/geek/springbootjsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.geek.springbootjsp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
